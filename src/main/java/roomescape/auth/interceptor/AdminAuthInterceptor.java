7187ce22530344a06c6d7b7d66f52a290d946721
Jinriro <riro12.hanyang.ac.kr>
2026-05-19 20:23:45 +0900

    refactor: 예외 명확히 구분

--- src/main/java/roomescape/auth/interceptor/AdminAuthInterceptor.java
+++ src/main/java/roomescape/auth/interceptor/AdminAuthInterceptor.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 import roomescape.auth.controller.AdminAuthController;
 import roomescape.exception.ForbiddenException;
+import roomescape.exception.UnauthorizedException;
 
 public class AdminAuthInterceptor implements HandlerInterceptor {
 
@@ -13,7 +14,11 @@ public class AdminAuthInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         HttpSession session = request.getSession(false);
 
-        if (session == null || session.getAttribute(AdminAuthController.ADMIN_SESSION_KEY) == null) {
+        if (session == null) {
+            throw new UnauthorizedException("로그인이 필요합니다.");
+        }
+
+        if (session.getAttribute(AdminAuthController.ADMIN_SESSION_KEY) == null) {
             throw new ForbiddenException("관리자 권한이 필요합니다.");
         }
 
