b4c648a7d929739c9a96e9546dfbb6438cfc209f
Jinriro <riro12.hanyang.ac.kr>
2026-05-19 19:45:13 +0900

    feat: reservation table에 member_id 로 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,35 +1,36 @@
 CREATE TABLE reservation_time (
-    id          BIGINT    NOT NULL AUTO_INCREMENT,
-    start_at    TIME      NOT NULL UNIQUE,
+    id         BIGINT    NOT NULL AUTO_INCREMENT,
+    start_at   TIME      NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE theme (
-    id               BIGINT           NOT NULL AUTO_INCREMENT,
-    name             VARCHAR(255)     NOT NULL UNIQUE,
-    description      VARCHAR(255)     NOT NULL,
-    thumbnail_url    VARCHAR(1024)    NOT NULL,
+    id                 BIGINT           NOT NULL AUTO_INCREMENT,
+    name               VARCHAR(255)     NOT NULL UNIQUE,
+    description        VARCHAR(255)     NOT NULL,
+    thumbnail_url      VARCHAR(1024)    NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE member (
     id          BIGINT          NOT NULL AUTO_INCREMENT,
-    login_id       VARCHAR(255)    NOT NULL UNIQUE,
+    login_id    VARCHAR(255)    NOT NULL UNIQUE,
     name        VARCHAR(50)     NOT NULL,
     password    VARCHAR(255)    NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation (
-    id                  BIGINT         NOT NULL AUTO_INCREMENT,
-    name                VARCHAR(50)    NOT NULL,
-    reservation_date    DATE           NOT NULL,
-    time_id             BIGINT         NOT NULL,
-    theme_id            BIGINT         NOT NULL,
-    status              VARCHAR(20)    NOT NULL DEFAULT 'RESERVED',
+    id                 BIGINT          NOT NULL AUTO_INCREMENT,
+    member_id          BIGINT          NOT NULL,
+    reservation_date   DATE            NOT NULL,
+    time_id            BIGINT          NOT NULL,
+    theme_id           BIGINT          NOT NULL,
+    status             VARCHAR(20)     NOT NULL DEFAULT 'RESERVED',
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id)     REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id)    REFERENCES theme (id),
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (time_id)  REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
     CONSTRAINT chk_reservation_status
         CHECK (status IN ('RESERVED', 'CANCELED'))
 );
